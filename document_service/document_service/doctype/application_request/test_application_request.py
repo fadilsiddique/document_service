6eadf9052c87ef0e2119e9a4c919d89c5f254ab6
fadilsid <fadil@tridz.com>
2023-11-16 18:25:39 +0000

    feat: document services uae

---
+++ document_service/document_service/doctype/application_request/test_application_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Upscape Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestApplicationRequest(FrappeTestCase):
+	pass
