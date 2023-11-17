644ccde5767adbc880f2c2d89211c0b93f7d64a0
fadilsid <fadil@tridz.com>
2023-11-17 10:57:38 +0000

    feat: download file from table

--- document_service/document_service/doctype/customer_document/customer_document.js
+++ document_service/document_service/doctype/customer_document/customer_document.js
@@ -1,8 +1,34 @@
 // Copyright (c) 2023, Upscape Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Customer Document", {
-// 	refresh(frm) {
+frappe.ui.form.on("Customer Document", {
+    // refresh(frm) {
 
-// 	},
-// });
+    // },
+});
+
+frappe.ui.form.on("Document Table", {
+
+    download: function (frm, cdt, cdn) {
+
+        let row = locals[cdt][cdn]
+        let url = row.attach_file
+
+        fetch(url)
+        .then(response => response.blob())
+        .then(blob => {
+            const link = document.createElement('a')
+            link.href = URL.createObjectURL(blob)
+
+            const fileName = url.substring(url.lastIndexOf('/') + 1);
+            link.download = fileName;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        })
+        .catch(error => {
+            console.error('Error downloading file:', error);
+        })
+
+    }
+})
